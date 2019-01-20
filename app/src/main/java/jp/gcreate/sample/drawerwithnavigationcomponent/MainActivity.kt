ef1aff8c7efd3e7798a61eea7093f874b3134475
Gen <archiherewego@gmail.com>
2019-01-20 12:08:53 +0900

    Implement navigation component

--- app/src/main/java/jp/gcreate/sample/drawerwithnavigationcomponent/MainActivity.kt
+++ app/src/main/java/jp/gcreate/sample/drawerwithnavigationcomponent/MainActivity.kt
@@ -1,27 +1,32 @@
 package jp.gcreate.sample.drawerwithnavigationcomponent
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import com.google.android.material.navigation.NavigationView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
+    
+    private lateinit var navController: NavController
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+    
+        navController = findNavController(this, R.id.nav_host_fragment)
         
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open,
             R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-        
-        nav_view.setNavigationItemSelectedListener(this)
+    
+        NavigationUI.setupWithNavController(nav_view, navController)
     }
     
     override fun onBackPressed() {
@@ -30,37 +35,5 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } else {
             super.onBackPressed()
         }
-    }
-    
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_camera    -> {
-                // Handle the camera action
-            }
-            
-            R.id.nav_gallery   -> {
-            
-            }
-            
-            R.id.nav_slideshow -> {
-            
-            }
-            
-            R.id.nav_manage    -> {
-            
-            }
-            
-            R.id.nav_share     -> {
-            
-            }
-            
-            R.id.nav_send      -> {
-            
-            }
-        }
-        
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
     }
 }
---
+++ app/src/main/java/jp/gcreate/sample/drawerwithnavigationcomponent/fragments/BottomFragment.kt
@@ -0,0 +1,17 @@
+package jp.gcreate.sample.drawerwithnavigationcomponent.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import jp.gcreate.sample.drawerwithnavigationcomponent.R
+
+class BottomFragment : Fragment() {
+    
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom, container, false)
+    }
+}
